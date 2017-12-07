2268bef9962c63166044166a0f8b18ad66cdd4ea
Yohan Lascombe <ylascombe@octo.com>
2017-12-08 00:32:51 +0100

    Debug bootstrap inclusion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { EnvListComponent } from './env-list/env-list.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EnvListComponent
+    EnvListComponent,
+    AppNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
