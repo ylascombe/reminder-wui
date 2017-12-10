8eb3cf5549e6da0245d4e02e772fdb6bc43ec3b3
Yohan Lascombe <ylascombe@octo.com>
2017-12-11 00:04:54 +0100

    Add more links to test and adjust style to display image correctly

--- src/app/mock-environments-detail.ts
+++ src/app/mock-environments-detail.ts
@@ -5,10 +5,29 @@ export const ENVIRONMENTS_DETAILS: EnvironmentDetail[] = [
     id: 1,
     name: 'INTEGRATION',
     urls: [
-      { title: 'Kibana',
-        url : 'http://es-elk-a-01:5601/',
+      {
+        title: 'Kibana',
+        url: 'http://es-elk-a-01:5601/',
         image_url: 'assets/kibana.svg',
         desc: 'Stack ELK pour exploiter les logs de la plateforme.'
+      },
+      {
+        title: 'Elasticsearch',
+        url: 'http://es-elk-a-01:9200/',
+        image_url: 'assets/elasticsearch.svg',
+        desc: 'Stack ELK pour exploiter les logs de la plateforme.'
+      },
+      {
+        title: 'Grafana',
+        url: 'http://grafana-a-01:9200/',
+        image_url: 'assets/grafana.svg',
+        desc: 'Outil de visualisation des metriques'
+      },
+      {
+        title: 'Prometheus',
+        url: 'http://prometheus-a-01:9200/',
+        image_url: 'assets/prometheus.png',
+        desc: 'Stockage temporel des métriques'
       }
     ]
   },
